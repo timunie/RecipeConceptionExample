4f2a69c21d617748395e2ec93240460883460607
Tim <tim.uniewski@gmail.com>
2020-06-18 12:38:09 +0200

    Implement Model and sqlite DB

---
+++ src/RecipeService_1/Model/Reciep.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeService_1.Model
+{
+    public class Reciep : BaseClass
+    {
+        public Reciep()
+        {
+            SaveCommand = new RelayCommand((param) => Save());
+        }
+
+        private long? _ID;
+        public long? ID
+        {
+            get { return _ID; }
+            set { _ID = value; RaisePropertyChanged(nameof(ID)); }
+        }
+
+        private string _Description;
+        public string Description
+        {
+            get { return _Description; }
+            set { _Description = value; RaisePropertyChanged(nameof(Description)); }
+        }
+
+
+       public RelayCommand SaveCommand { get; }
+        public void Save()
+        {
+            using (var conn = new SQLiteConnection(ViewModel.ConnectionString, true))
+            {
+                conn.Open();
+                var cmd = conn.CreateCommand();
+
+                cmd.CommandText = "REPLACE INTO recieps(ID, Description) VALUES(@ID, @Description);";
+
+                cmd.Parameters.AddWithValue("@ID", ID);
+                cmd.Parameters.AddWithValue("@Description", Description);
+
+                cmd.ExecuteNonQuery();
+
+                ID = conn.LastInsertRowId;
+
+                conn.Close();
+            } 
+            
+        }
+    }
+}
---
+++ src/RecipeService_1/Model/ViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeService_1.Model
+{
+    public class ViewModel : BaseClass
+    {
+        public static string ConnectionString = $"Data Source=\"{Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "DB", "reciep.db")}\";Version=3;FailIfMissing=True;";
+
+        public ObservableCollection<Reciep> Recieps { get; } = new ObservableCollection<Reciep>();
+    }
+}
